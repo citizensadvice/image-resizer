57e4cdd793af6bb94f56da1cae62da0e8b1ed8bd
rdsngit <52106678+rdsngit@users.noreply.github.com>
2020-10-01 12:47:26 +0100

    add test paths for png and tiff

--- app/app.rb
+++ app/app.rb
@@ -8,6 +8,20 @@ get '/' do
   body 'Hello world!'
 end
 
+get 'png' do
+	image_file = File.open('./test_images/test-png-1102x1287px.png')
+	resized_image = ImageResizeService.call(image_file)
+	status 200
+	body resized_image
+end
+
+get 'tiff' do
+	image_file = File.open('./test_images/test-png-1102x1287px.png')
+	resized_image = ImageResizeService.call(image_file)
+	status 200
+	body resized_image
+end
+
 get '/resize_image' do
 	if params.key?(:image)
 		resized_image = ImageResizeService.call(params[:image])
