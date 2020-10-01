df0b9ea22f71537ef14919502e7b2231b56e745e
rdsngit <52106678+rdsngit@users.noreply.github.com>
2020-10-01 12:55:35 +0100

    Add mime_type params to routes

--- app/app.rb
+++ app/app.rb
@@ -8,23 +8,23 @@ get '/' do
   body 'Hello world!'
 end
 
-get 'png' do
+get '/test/png' do
 	image_file = File.open('./test_images/test-png-1102x1287px.png')
-	resized_image = ImageResizeService.call(image_file)
+	resized_image = ImageResizeService.call(image_file, 'image/png')
 	status 200
 	body resized_image
 end
 
-get 'tiff' do
+get '/test/tiff' do
 	image_file = File.open('./test_images/test-png-1102x1287px.png')
-	resized_image = ImageResizeService.call(image_file)
+	resized_image = ImageResizeService.call(image_file, 'image/tiff')
 	status 200
 	body resized_image
 end
 
 get '/resize_image' do
-	if params.key?(:image)
-		resized_image = ImageResizeService.call(params[:image])
+	if params.key?(:image) && params.key?(:mime_type)
+		resized_image = ImageResizeService.call(params[:image], params[:mime_type])
 		status 200
 		body resized_image
 	else
