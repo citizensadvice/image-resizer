f09f925350dc1bf6d06ea9789dc6cf17904c5cb3
rdsngit <52106678+rdsngit@users.noreply.github.com>
2020-10-01 12:29:49 +0100

    Add code to process image in image resizer app file

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,20 @@
 require 'sinatra'
+require './image_resizer.rb'
+
+set :show_exceptions, :after_handler
 
 get '/' do
   status 200
   body 'Hello world!'
+end
+
+get '/resize_image' do
+	if params.key?(:image)
+		resized_image = ImageResizeService.call(params[:image])
+		status 200
+		body resized_image
+	else
+		status 400
+		body 'Bad request or missing param for image'
+	end
 end
