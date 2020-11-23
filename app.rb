e0567f391a889e453a38c646f4feea70d16b922a
rdsngit <52106678+rdsngit@users.noreply.github.com>
2020-11-23 15:40:23 +0000

    refactor tests to use response variable

--- app.rb
+++ app.rb
@@ -11,13 +11,13 @@ get "/" do
 end
 
 post "/image" do
-  unless params[:image] && params[:mime_type]
+  unless params.keys == %w[image mime_type]
     status 400
     return ArgumentError.new("expected params to include :mime_type, :image")
   end
 
-  mime_type = params[:mime_type]
-  image_file = params[:image][:tempfile]
+  mime_type = params["mime_type"]
+  image_file = params["image"]["tempfile"]
 
   resized_image = ImageResizeService.call(image_file, mime_type)
 
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -20,18 +20,18 @@ describe "image resizer app" do
   describe "post /image" do
     context "without params" do
       it "returns status code 400" do
-        post "/image"
-        expect(last_response.status).to eq 400
+        response = post "/image"
+        expect(response.status).to eq 400
       end
     end
 
-    xcontext "with image file .png" do
-      let!(:image) { File.open("spec/fixtures/image/test-png.png") }
-      let!(:mime_type) { "image/png" }
+    context "with image file .png" do
+      let!(:image) { File.open("./spec/fixtures/image/test-png.png") }
+      let(:mime_type) { "image/png" }
 
-      it "returns a resized image" do
-        post "/image", image: image, mime_type: mime_type
-        expect(last_response.status).to eq 400
+      xit "returns status code 200" do
+        response = post "/image", image: image, mime_type: mime_type
+        expect(response.status).to eq 200
       end
     end
   end
