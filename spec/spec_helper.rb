e4baa992647e820a89ac7d8921b20889f35db09e
rdsngit <52106678+rdsngit@users.noreply.github.com>
2020-11-23 17:49:35 +0000

    add rack test to rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,18 @@
 
 require "byebug"
 require "capybara/rspec"
+require "rack/test"
 require "sinatra"
 require_relative "../app"
 
 Capybara.app = Sinatra::Application
+
+def app
+  Sinatra::Application
+end
+
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+end
+
+
